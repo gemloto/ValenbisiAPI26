fcda1dd02a3d1bd17df7e565e761ddcf73493753
Gema Lozano <gemloztor@alu.edu.gva>
2026-04-22 00:53:58 +0200

    Tarea 1 y 2 del Proyecto Intermodular

---
+++ src/main/java/es/gva/edu/iesjuandegaray/bicis/Geometry.java
@@ -0,0 +1,6 @@
+package es.gva.edu.iesjuandegaray.bicis;
+
+public class Geometry {
+	public double x;
+    public double y;
+}
---
+++ src/main/java/es/gva/edu/iesjuandegaray/bicis/ValenbisiAPI26T1v2_1.java
@@ -0,0 +1,78 @@
+package es.gva.edu.iesjuandegaray.bicis;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class ValenbisiAPI26T1v2_1 {
+
+	// https://geoportal.valencia.es/server/rest/services/OPENDATA/Trafico/MapServer/228/query?where=1=1&outFields=*&f=json
+    private static final String API_URL =
+            "https://geoportal.valencia.es/server/rest/services/OPENDATA/Trafico/MapServer/228/query"
+            + "?where=1%3D1"
+            + "&outFields=*"
+            + "&returnGeometry=true"
+            + "&f=json";
+
+    public static void main(String[] args) {
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+
+            HttpGet request = new HttpGet(API_URL);
+            HttpResponse response = httpClient.execute(request);
+
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+
+                String result = EntityUtils.toString(entity);
+                
+                // Convertimos a JSON
+                JSONObject jsonObject = new JSONObject(result);
+                
+                // Obtenemos el array "features"
+                JSONArray features = jsonObject.getJSONArray("features");
+
+                System.out.println("Número de estaciones: " + features.length());
+                System.out.println();
+                
+                // BUCLE RECORRE VECTOR FEATURES MOSTRANDO LOS DATOS SOLICITADOS.
+                for (int i = 0; i < features.length(); i++) {
+                    JSONObject estacion = features.getJSONObject(i);
+
+                    JSONObject attributes = estacion.getJSONObject("attributes");
+                    JSONObject geometry = estacion.getJSONObject("geometry");
+
+                    String direccion = attributes.optString("direccion", "Sin dirección");
+                    int bicicletasDisponibles = attributes.optInt("bicis_disponibles", 0);
+                    int anclajesLibres = attributes.optInt("anclajes_libres", 0);
+                    int anclajesTotales = attributes.optInt("numero_anclajes", 0);
+                    double x = geometry.optDouble("x", 0.0);
+                    double y = geometry.optDouble("y", 0.0);
+
+                    System.out.println("Estación " + (i + 1));
+                    System.out.println("Dirección: " + direccion);
+                    System.out.println("Bicicletas disponibles: " + bicicletasDisponibles);
+                    System.out.println("Anclajes libres: " + anclajesLibres);
+                    System.out.println("Anclajes totales: " + anclajesTotales);
+                    System.out.println("Coordenadas: X = " + x + ", Y = " + y);
+                    System.out.println("------------------------------------");
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error en la petición HTTP:");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Error procesando JSON:");
+            e.printStackTrace();
+        }
+    }
+}
